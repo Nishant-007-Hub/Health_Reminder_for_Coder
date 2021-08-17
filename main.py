ef55a8aed5e7d3d6ec334fb15780a17497d2bb15
Nishant <kabariyanishant@gmail.com>
2021-08-17 15:45:45 +0530

    working codebase

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
-import playsound
+from time import time
+from playsound import playsound
+import datetime
+import multiprocessing
 
-playsound.playsound("C:\\Users\\Nishant\\Music\\TeraGhata.mp3")
+def log_(msg):
+    with open("mylogs.txt", 'a')as f:
+        f.write(f"{msg}{datetime.datetime.now()}\n")
+
+
+if __name__ == '__main__':
+    init_water = time()
+    init_eyes = time()
+    init_exercise = time()
+
+    watersec = 30 * 60
+    eyesec = 20 * 60
+    exercisesec = 40 * 60
+
+    while True:
+        if time() - init_water > watersec:
+            print("water drinking time...")
+            log_("water done at::\t")
+            var = multiprocessing.Process(target=playsound, args=("water.mp3",))
+            var.start()
+            input("Enter any key to stop\n")
+            var.terminate()
+            init_water = time()
+
+        if time() - init_eyes > eyesec:
+            print("Eyes Exercise time...")
+            log_("done Eyes at::\t")
+            var = multiprocessing.Process(target=playsound, args=("eyes.mp3",))
+            var.start()
+            input("Enter any key to stop\n")
+            var.terminate()
+            init_eyes = time()
+
+        if time() - init_exercise > exercisesec:
+            print("Physical Exercise time...")
+            log_("done Exercise at::\t")
+            var = multiprocessing.Process(target=playsound, args=("exercise.mp3",))
+            var.start()
+            input("Enter any key to stop\n")
+            var.terminate()
+            init_exercise = time()
